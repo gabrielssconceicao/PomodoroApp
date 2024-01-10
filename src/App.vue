ba7eb12a13ce5fa679df5d7d59b17c45cf4bdcd8
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-01-10 12:27:04 -0300

    pomodorotimercomponent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import PomodoroTimer from './components/PomodoroTimerComponent.vue';
 
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <PomodoroTimer :main-time="1200" />
 </template>
 
 <style scoped></style>
---
+++ src/components/PomodoroTimerComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useInterval } from '@/hooks/use-interval';
+import { ref } from 'vue';
+const props = defineProps({
+  mainTime: {
+    type: Number,
+    required: true
+  }
+})
+const time = ref(props.mainTime)
+useInterval(() => time.value -= 1, 1000)
+</script>
+<template>
+  <div>{{ time }}</div>
+</template>
+<style scoped></style>
